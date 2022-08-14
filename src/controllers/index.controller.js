9a8be9ffb76ffb704881a208f6e6b96e62dbc7fe
NeilVm <neilerickson512@gmail.com>
2022-08-13 22:43:08 -0500

    b

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,9 @@
-const {Pool} = require('pg')
-const connectionString =  'postgres://ckpqmstmomyhow:11f2bf1238a592032bbd28aedf39d3a65b04f91a427866031a030d1f3e6b3d32@ec2-44-193-178-122.compute-1.amazonaws.com:5432/d88nit0ug81m05'
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://nyxxhahekqyrnm:d42a0f7252d1f103fe296fd4d2386fb8b57ce19a13dce7a16433e421fa8faa1e@ec2-34-193-44-192.compute-1.amazonaws.com:5432/d8hiluub25otjj'
 
-const pool = new Pool({connectionString})
+const pool = new Pool({
+  connectionString,
+})
 
 
 
